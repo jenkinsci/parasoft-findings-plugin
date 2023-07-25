021effc76792543049241d8b507ac06c3ec43778
lli-parasoft <lli-parasoftA@Email.com>
2023-07-25 11:15:36 +0800

    Extract duplicate code, change the order of test cases.

--- src/test/java/com/parasoft/findings/jenkins/coverage/ParasoftCoverageRecorderTest.java
+++ src/test/java/com/parasoft/findings/jenkins/coverage/ParasoftCoverageRecorderTest.java
@@ -1,6 +1,5 @@
 package com.parasoft.findings.jenkins.coverage;
 
-import hudson.Launcher;
 import hudson.model.*;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
@@ -14,61 +13,27 @@ import org.mockito.Mockito;
 import static org.junit.Assert.assertEquals;
 
 public class ParasoftCoverageRecorderTest {
-    Launcher launcher;
-    BuildListener listener;
-
     @Rule
     public JenkinsRule jenkinsRule = new JenkinsRule();
-
-    @Test
-    public void testNotFindCoverageFile() throws Exception {
-        ParasoftCoverageRecorder parasoftCoverageRecorder = new ParasoftCoverageRecorder();
-        FreeStyleProject project = jenkinsRule.createFreeStyleProject();
-
-        project.getPublishersList().add(parasoftCoverageRecorder);
-        FreeStyleBuild build = project.scheduleBuild2(0).get();
-        String log = FileUtils.readFileToString(build.getLogFile(), "UTF-8");
-        Assert.assertTrue(log.contains("[Parasoft Coverage] [-ERROR-] No files found for pattern '**/coverage.xml'. Configuration error?"));
-        Assert.assertTrue(log.contains("Finished: FAILURE"));
-    }
-
-    @Test
-    public void testNoWorkspace() {
-        ParasoftCoverageRecorder parasoftCoverageRecorder = new ParasoftCoverageRecorder();
-        AbstractBuild<?, ?> build = Mockito.mock(AbstractBuild.class);
-        Mockito.when(build.getWorkspace()).thenReturn(null);
-        String message = "";
-        try {
-            parasoftCoverageRecorder.perform(build, launcher, listener);
-        } catch (Exception e) {
-            message = e.getMessage();
-        }
-        assertEquals("No workspace found for " + build, message);
-    }
+    static String success = "Finished: SUCCESS";
+    static String failure = "Finished: FAILURE";
+    static String usingDefaultPattern = "[Parasoft Coverage] Using default pattern '**/coverage.xml' for '' since specified pattern is empty";
 
     @Test
     public void testUsingSetPattern() throws Exception {
         ParasoftCoverageRecorder parasoftCoverageRecorder = new ParasoftCoverageRecorder();
-        FreeStyleProject project = jenkinsRule.createFreeStyleProject();
-        project.setScm(new SingleFileSCM("coverage.xml", getClass().getResource("/coverage.xml")));
         parasoftCoverageRecorder.setPattern("**/coverage.xml");
-        project.getPublishersList().add(parasoftCoverageRecorder);
-        FreeStyleBuild build = project.scheduleBuild2(0).get();
-        String log = FileUtils.readFileToString(build.getLogFile(), "UTF-8");
-        Assert.assertFalse(log.contains("[Parasoft Coverage] Using default pattern '**/coverage.xml' for '' since specified pattern is empty"));
-        Assert.assertTrue(log.contains("Finished: SUCCESS"));
+        String log = runPlugin(parasoftCoverageRecorder, true);
+        Assert.assertFalse(log.contains(usingDefaultPattern));
+        Assert.assertTrue(log.contains(success));
     }
 
     @Test
     public void testUsingDefaultPattern() throws Exception {
         ParasoftCoverageRecorder parasoftCoverageRecorder = new ParasoftCoverageRecorder();
-        FreeStyleProject project = jenkinsRule.createFreeStyleProject();
-        project.setScm(new SingleFileSCM("coverage.xml", getClass().getResource("/coverage.xml")));
-        project.getPublishersList().add(parasoftCoverageRecorder);
-        FreeStyleBuild build = project.scheduleBuild2(0).get();
-        String log = FileUtils.readFileToString(build.getLogFile(), "UTF-8");
-        Assert.assertTrue(log.contains("[Parasoft Coverage] Using default pattern '**/coverage.xml' for '' since specified pattern is empty"));
-        Assert.assertTrue(log.contains("Finished: SUCCESS"));
+        String log = runPlugin(parasoftCoverageRecorder, true);
+        Assert.assertTrue(log.contains(usingDefaultPattern));
+        Assert.assertTrue(log.contains(success));
     }
 
     @Test
@@ -79,7 +44,39 @@ public class ParasoftCoverageRecorderTest {
         project.getPublishersList().add(parasoftCoverageRecorder);
         FreeStyleBuild build = project.scheduleBuild2(0).get();
         String log = FileUtils.readFileToString(build.getLogFile(), "UTF-8");
-        Assert.assertTrue(log.contains("[Parasoft Coverage] [-ERROR-] Errors while recording Parasoft code coverage:"));
-        Assert.assertTrue(log.contains("Finished: FAILURE"));
+        Assert.assertTrue(log.contains("[Parasoft Coverage] [-ERROR-] Skipping file 'coverage.xml' because it's empty"));
+        Assert.assertTrue(log.contains(failure));
+    }
+
+    @Test
+    public void testNotFoundCoverageFile() throws Exception {
+        ParasoftCoverageRecorder parasoftCoverageRecorder = new ParasoftCoverageRecorder();
+        String log = runPlugin(parasoftCoverageRecorder, false);
+        Assert.assertTrue(log.contains("[Parasoft Coverage] [-ERROR-] No files found for pattern '**/coverage.xml'. Configuration error?"));
+        Assert.assertTrue(log.contains(failure));
+    }
+
+    @Test
+    public void testNoWorkspace() {
+        ParasoftCoverageRecorder parasoftCoverageRecorder = new ParasoftCoverageRecorder();
+        AbstractBuild<?, ?> build = Mockito.mock(AbstractBuild.class);
+        Mockito.when(build.getWorkspace()).thenReturn(null);
+        String message = "";
+        try {
+            parasoftCoverageRecorder.perform(build, null, null);
+        } catch (Exception e) {
+            message = e.getMessage();
+        }
+        assertEquals("No workspace found for " + build, message);
+    }
+
+    private String runPlugin(ParasoftCoverageRecorder parasoftCoverageRecorder, boolean setXML) throws Exception {
+        FreeStyleProject project = jenkinsRule.createFreeStyleProject();
+        if(setXML) {
+            project.setScm(new SingleFileSCM("coverage.xml", getClass().getResource("/coverage.xml")));
+        }
+        project.getPublishersList().add(parasoftCoverageRecorder);
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+        return FileUtils.readFileToString(build.getLogFile(), "UTF-8");
     }
 }
